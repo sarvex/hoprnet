1c735d1751009fe677e09c0e59fa95dd5899e7b1
HOPR CI robot <noreply@hoprnet.org>
2022-08-11 07:50:40 +0000

    chore(build): apply linter changes

--- packages/core-ethereum/src/db/interfaces.ts
+++ packages/core-ethereum/src/db/interfaces.ts
@@ -1,5 +1,10 @@
-import { PriorityConfiguration, StatusTransacitons, Transaction, TransactionPayload } from "../transaction-manager/types.js";
-import { TransactionHash } from "./types.js";
+import {
+  PriorityConfiguration,
+  StatusTransacitons,
+  Transaction,
+  TransactionPayload
+} from '../transaction-manager/types.js'
+import { TransactionHash } from './types.js'
 
 /**
  * Interface of CoreEthereumDB
@@ -14,34 +19,34 @@ export interface ICoreEthereumDB {
   // pop the first queuing transaction
   popFirstQueuingTransaction(): TransactionPayload
 
-    /**
-     * add a boardcasted transaction. This transaction should be defautl to pending
-     * 
-     * Note that this function is also called when updating a transaction's network priority condition 
-     * - a new entry in the Boardcasted transaction queue should be created. The old hash entry is not removed.
-     * Add the new hash associated with the nonce
-     * @param transaction 
-     */
-    addBoardcastedTransaction(transaction: Transaction): void
-    
-    /**
-     * dedicated function for updating the priority config. See required actions
-     * in the `addBoardcastedTransaction` function.
-     */
-    updateTransactionPriorityConfig(prevHash: TransactionHash, priorityConfig: PriorityConfiguration): void
-    
-    // return a transaction by its hash
-    getBoardcastedTransactionByHash(hash: TransactionHash): Transaction
-    /*
-     * udpate a transaction by its hash. When multiple hashes are associated with one nonce. Only one
-     * transaction can be `FINALIZED` and all the rest should be `REPLACED`
-     * @param hash of the  transaction
-     * @param newStatus new status
-     */
-    updateBoardcastedTransactionStatusByHash(hash: TransactionHash, newStatus: StatusTransacitons): void
+  /**
+   * add a boardcasted transaction. This transaction should be defautl to pending
+   *
+   * Note that this function is also called when updating a transaction's network priority condition
+   * - a new entry in the Boardcasted transaction queue should be created. The old hash entry is not removed.
+   * Add the new hash associated with the nonce
+   * @param transaction
+   */
+  addBoardcastedTransaction(transaction: Transaction): void
 
-    /**
-     * return an array of transactions based on the given transaction status
-     */
-     getBoardcastedTransactionsByStatus(status: StatusTransacitons): Array<Transaction>
+  /**
+   * dedicated function for updating the priority config. See required actions
+   * in the `addBoardcastedTransaction` function.
+   */
+  updateTransactionPriorityConfig(prevHash: TransactionHash, priorityConfig: PriorityConfiguration): void
+
+  // return a transaction by its hash
+  getBoardcastedTransactionByHash(hash: TransactionHash): Transaction
+  /*
+   * udpate a transaction by its hash. When multiple hashes are associated with one nonce. Only one
+   * transaction can be `FINALIZED` and all the rest should be `REPLACED`
+   * @param hash of the  transaction
+   * @param newStatus new status
+   */
+  updateBoardcastedTransactionStatusByHash(hash: TransactionHash, newStatus: StatusTransacitons): void
+
+  /**
+   * return an array of transactions based on the given transaction status
+   */
+  getBoardcastedTransactionsByStatus(status: StatusTransacitons): Array<Transaction>
 }
