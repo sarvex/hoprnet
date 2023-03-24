507f76eb85af02373fe72a6207fdad3b8d5be04c
Tino Breddin <t@tolbrino.com>
2023-03-24 11:02:10 +0100

    API: Fix bug in token identity endpoint to return all associated capabilities

--- packages/hoprd/src/api/v2/paths/token.integration.spec.ts
+++ packages/hoprd/src/api/v2/paths/token.integration.spec.ts
@@ -1,3 +1,4 @@
+import assert from 'assert'
 import request from 'supertest'
 import sinon from 'sinon'
 import chaiResponseValidator from 'chai-openapi-response-validator'
@@ -27,13 +28,13 @@ describe('GET /token', function () {
 
   it('should fail with not found error when using superuser token', async function () {
     const res = await request(service).get('/api/v2/token').set('x-auth-token', 'superuser')
-    expect(res.status).to.equal(404)
+    assert.equal(res.status, 404)
     expect(res).to.satisfyApiSpec
   })
 
   it('should fail with unauthenticated error when using no token', async function () {
     const res = await request(service).get('/api/v2/token')
-    expect(res.status).to.equal(401)
+    assert.equal(res.status, 401)
     expect(res).to.satisfyApiSpec
   })
 
@@ -44,18 +45,31 @@ describe('GET /token', function () {
     await storeToken(node.db, token)
 
     const res = await request(service).get('/api/v2/token').set('x-auth-token', token.id)
-    expect(res.status).to.equal(403)
+    assert.equal(res.status, 403)
     expect(res).to.satisfyApiSpec
   })
 
   it('should succeed when using token with correct capability', async function () {
     // create token with correct capability
-    const caps = [{ endpoint: 'tokensGetToken' }]
-    const token = await createToken(node.db, undefined, caps)
-    await storeToken(node.db, token)
+    const caps = [{ endpoint: 'tokensCreate' }, {endpoint: 'tokensGetToken'}]
+    const token1 = await createToken(node.db, undefined, caps)
+    await storeToken(node.db, token1)
 
-    const res = await request(service).get('/api/v2/token').set('x-auth-token', token.id)
-    expect(res.status).to.equal(200)
-    expect(res).to.satisfyApiSpec
+    const res1 = await request(service).get('/api/v2/token').set('x-auth-token', token1.id)
+    assert.equal(res1.status, 200)
+    expect(res1).to.satisfyApiSpec
+    assert(res1.body.capabilities, "capabilities should not be missing")
+    assert.equal((res1.body.capabilities.length), 2, "capabilities should have 2 entries")
+    assert(!!!res1.body.valid_until, "valid_until should be missing")
+
+    const token2 = await createToken(node.db, undefined, caps, "", 1000)
+    await storeToken(node.db, token2)
+
+    const res2 = await request(service).get('/api/v2/token').set('x-auth-token', token2.id)
+    assert.equal(res2.status, 200)
+    expect(res2).to.satisfyApiSpec
+    assert(res2.body.capabilities, "capabilities should not be missing")
+    assert.equal((res2.body.capabilities.length), 2, "capabilities should have 2 entries")
+    assert(!!res2.body.valid_until, "valid_until should not be missing")
   })
 })
